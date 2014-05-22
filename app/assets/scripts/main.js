5a1028f8eca3f76fe8b1f47763fee8cb8c3b6a9e
aldenaik <aldenaikele@gmail.com>
2014-05-21 20:36:32 -0700

    working draft

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,112 +1,335 @@
 $(document).ready(function() {
+    document.getElementById("leftTitle").innerHTML = screenCarousel[0].imgTitle;
+    document.getElementById("middleTitle").innerHTML = screenCarousel[1].imgTitle;
+    document.getElementById("rightTitle").innerHTML = screenCarousel[2].imgTitle;
+        var  lastClickedElement="";
 
+    $( ".right, #rightTitle" ).mouseup(function(e) {       //Right Click
+        $("#rightPlacement").css("-webkit-animation","none");  //turns off openeing animation of hands
+        $(".cloudExplain").css("-webkit-animation","none");  //turns off opening animation of cloud
 
-//    JSON.parse(screenCarousel);
-//    alert(screenCarousel.count);
-
-
-//
-//    document.getElementById("placeholder").innerHTML=screenCarousel[0];
-//    console.log(placeholder);
-
-
-
-
-//    function load() {
-//        var mydata = JSON.parse(carouselData);
-////        console.log(mydata[0])    }
-//    $.ajax({
-//
-//        url: 'https://raw.githubusercontent.com/aldenaik/slideshow/aadevelopment/app/assets/scripts/carouselData.js',
-//        type: 'GET',
-////        crossDomain: true,
-//        dataType: 'json',
-//        success: function() { alert("Success"); },
-//        error: function() { alert('Failed!'); },
-////        beforeSend: "https://render.githubusercontent.com"
-//    });
-
-
-//    function requestJSON(url, callback) {
-//        $.ajax({
-//            url: "https://raw.githubusercontent.com/aldenaik/slideshow/aadevelopment/app/assets/scripts/carouselData.js",
-//            complete: function(xhr) {
-//                callback.call(null, xhr.responseJSON);
-//            }
-//        });
-//    }
-
-
-
-        $.getJSON("https://raw.githubusercontent.com/aldenaik/slideshow/aadevelopment/app/assets/scripts/carouselData.js", function(data) {   //inside the function and the console.log I have also tried inserting screenCarousel
-            console.log(data);
-
-//            var screenCarousel = [
-//                {
-//                    "imgSrc":"http://www.zendesk.com/public/assets/images/p-helpcenter/screen-portal.png",
-//                    "imgTitle":"Knowledge Base",
-//                    "imgDesc":"Faster answers means happier customers. Deflect costly, repetitive inquiries and drive support efficiencies with a knowledge base. Organize your content—in 40+ languages—so everything is easy to find.",
-//                    "imgPosTop":"199px",
-//                    "imgPosLeft":"255px"
-//                },
-//                {
-//                    "imgSrc":"http://www.zendesk.com/public/assets/images/p-helpcenter/screen-community.png",
-//                    "imgTitle":"Community",
-//                    "imgDesc":"Grow deeper roots in your business community by opening a dialogue and building relationships outside of the support ticket. Start a community discussion to gather feedback and crowdsource ideas. Your customers can also post a question and get answers from their peers.",
-//                    "imgPosTop":"80px",
-//                    "imgPosLeft":"145px"
-//                },
-//                {
-//                    "imgSrc":"http://www.zendesk.com/public/assets/images/p-helpcenter/screen-knowledge-base.png",
-//                    "imgTitle":"Customer Portal",
-//                    "imgDesc":"Did you know customers actually prefer self-service? Give your customers a single destination for everything they need. One click on My Activities brings up their entire support history. A single search surfaces content from both the knowledge base and community.",
-//                    "imgPosTop":"270px",
-//                    "imgPosLeft":"450px"
-//                }
-//            ]
-
-
-        });
-
-
-    $( ".right" ).click(function() {
-        $(".rightFlick").show();
-            setTimeout(function() {
-                $(".rightFlick").hide();
-                        },225);
+        $(".rightFlick").show();               //control snapping fingers
+        setTimeout(function() {
+            $(".rightFlick").hide();
+        },175);
 
         $(".right").hide();
         setTimeout(function() {
             $(".right").show();
-        },225);
+        },175);                //end snapping fingers
+        $(".cloudRight").hide();
         setTimeout(function() {
-        $(".firstSlide").css('background-image', "url('http://www.zendesk.com/public/assets/images/p-helpcenter/screen-knowledge-base.png')");
-        },0);
+            $(".cloudRight").fadeIn(1000);
+        },500);
+        //if else helps determine the order of objects being pulled from array
+        if(($(window.lastClickedElement).attr('class')) == "left"){  //if yes, it means you just clicked left hand before right
+//            console.log(window.lastClickedElement.attr)
+
+            var active = screenCarousel.pop();
+            screenCarousel.unshift(active)
+            var active = screenCarousel.pop();                //second shift so that it gets the correctly ordered object from array
+
+            //pull out info from active object
+            document.getElementById('imageActive').src = active.imgSrc;
+            document.getElementById('imageActive').className = "";
+            setTimeout(function() {
+                var delayImage = document.getElementById('imageActive').className = "activeR";
+                delayImage;
+            },175);
+
+            document.getElementById("middleTitle").innerHTML = active.imgTitle;
+            document.getElementById("cloudTitle").innerHTML = active.imgTitle;
+            document.getElementById("leftTitle").innerHTML = screenCarousel[0].imgTitle;
+            document.getElementById("rightTitle").innerHTML = screenCarousel[1].imgTitle;
+            document.getElementById("cloudDescription").innerHTML = active.imgDesc;
+            document.getElementById("cloud").style.top=active.imgPosTop;
+            document.getElementById("cloud").style.left=active.imgPosLeft;
 
 
-    });
-    $( ".left" ).click(function() {
-        $(".leftFlick").show();
+
+//                    $("#middle").text("My new text!");
+            //put active object at the end of the array
+            screenCarousel.unshift(active)
+        }
+
+        else{                          //else yes, it means you did not click left hand before right
+
+            var active = screenCarousel.pop();
+            //pulls out info from JSON file for Markup
+            document.getElementById('imageActive').src = active.imgSrc;
+            document.getElementById("cloudDescription").innerHTML = active.imgDesc;
+            document.getElementById("cloud").style.top=active.imgPosTop;
+            document.getElementById("cloud").style.left=active.imgPosLeft;
+            document.getElementById("cloudTitle").innerHTML = active.imgTitle;
+
+            //Adds Removes active class for css transitions main image
+            document.getElementById('imageActive').className = "";
+            setTimeout(function() {
+                var delayImage = document.getElementById('imageActive').className = "activeR";
+                delayImage;
+            },1);
+
+            //Adds Removes active class for css transitions middleTitle
+            document.getElementById("middleTitle").innerHTML = active.imgTitle;
+            document.getElementById('middleTitle').className = "";
+            setTimeout(function() {
+                var delayMTitle = document.getElementById('middleTitle').className = "active";
+                delayMTitle;
+            },130);
+
+
+            //Adds Removes active class for css transitions leftTitle
+            document.getElementById("leftTitle").innerHTML = screenCarousel[0].imgTitle;
+            document.getElementById('leftTitle').className = "";
+            setTimeout(function() {
+                var delayMTitle = document.getElementById('leftTitle').className = "active";
+                delayMTitle;
+
+            },130);
+
+            //Adds Removes active class for css transitions rightTitle
+            document.getElementById("rightTitle").innerHTML = screenCarousel[1].imgTitle;
+            document.getElementById('rightTitle').className = "";
+            setTimeout(function() {
+                var delayMTitle = document.getElementById('rightTitle').className = "active";
+                delayMTitle;
+            },130);
+
+            screenCarousel.unshift(active)
+        }
+            //this event helps to keep track of which hand was last clicked
+            e = e || event;
+            $.lastClicked = e.target || e.srcElement;
+            var lastClickedElement = $.lastClicked;
+            window.lastClickedElement=lastClickedElement;
+
+    })                                          //end right hand Click
+
+
+    $( ".left,#leftTitle" ).mouseup(function(e) {        //Left Click
+
+        $("#leftPlacement").css("-webkit-animation","none");  //turns off opening animation of hands
+        $(".cloudExplain").css("-webkit-animation","none");  //turns off opening animation of cloud
+
+        $(".leftFlick").show();             //control snapping fingers
         setTimeout(function() {
             $(".leftFlick").hide();
-        },225);
+        },175);
         $(".left").hide();
         setTimeout(function() {
             $(".left").show();
-        },225);
-        $(".firstSlide").css('background-image', "url('http://www.zendesk.com/public/assets/images/p-helpcenter/screen-community.png')");
-    });
+        },175);
+        //end snapping fingers
+
+        $(".cloudRight").hide();
+        setTimeout(function() {
+            $(".cloudRight").fadeIn(1000);
+        },500);
+
+        //if else helps determine the order of objects being pulled from array
+        if(($(window.lastClickedElement).attr('class')) == "right"){   //if yes, it means you just clicked right hand before left
+//            console.log(screenCarousel)
+
+            var active = screenCarousel.shift();
+            screenCarousel.push(active)
+            var active = screenCarousel.shift();         //second pop so that it gets the correctly ordered object from array
+//            console.log(screenCarousel)
+//            console.log(active)
+
+            //pull out info from active object and put in id for markup
+            document.getElementById('imageActive').src = active.imgSrc; //active image
+            document.getElementById('imageActive').className = "";
+            setTimeout(function() {
+                var delayImage = document.getElementById('imageActive').className = "activeL";
+                delayImage;
+            },175);
+
+            document.getElementById("cloudTitle").innerHTML = active.imgTitle;
+            document.getElementById("middleTitle").innerHTML = active.imgTitle;  //active title
+            document.getElementById("leftTitle").innerHTML = screenCarousel[0].imgTitle; //left Title
+            document.getElementById("rightTitle").innerHTML = screenCarousel[1].imgTitle; //right Title
+            document.getElementById("cloudDescription").innerHTML = active.imgDesc; //Active Description
+            document.getElementById("cloud").style.top=active.imgPosTop;     //Cloud positioning
+            document.getElementById("cloud").style.left=active.imgPosLeft;   //Description positioning
+
+            //put active object at the end of the array
+            screenCarousel.push(active)
+//            console.log(screenCarousel)
+
+        }
+        else{                           //else yes, it means you did not click right hand before left
+
+            var active = screenCarousel.shift();
+
+                                        //pulls out info from JSON file for Markup
+            document.getElementById('imageActive').src = active.imgSrc;
+            document.getElementById("cloudDescription").innerHTML = active.imgDesc;
+            document.getElementById("cloud").style.top=active.imgPosTop;
+            document.getElementById("cloud").style.left=active.imgPosLeft;
+            document.getElementById("cloudTitle").innerHTML = active.imgTitle;
+
+            //Adds Removes active class for css transitions main image
+            document.getElementById('imageActive').className = "";
+            setTimeout(function() {
+                var delayImage = document.getElementById('imageActive').className = "activeL";
+                delayImage;
+            },1);
+
+            //Adds Removes active class for css transitions middleTitle
+            document.getElementById("middleTitle").innerHTML = active.imgTitle;
+            document.getElementById('middleTitle').className = "";
+            setTimeout(function() {
+                var delayMTitle = document.getElementById('middleTitle').className = "active";
+                delayMTitle;
+            },175);
 
 
-//        var url = '../scripts/carouselData.js';
+            //Adds Removes active class for css transitions leftTitle
+            document.getElementById("leftTitle").innerHTML = screenCarousel[0].imgTitle;
+            document.getElementById('leftTitle').className = "";
+            setTimeout(function() {
+                var delayMTitle = document.getElementById('leftTitle').className = "active";
+                delayMTitle;
+
+            },175);
+
+            //Adds Removes active class for css transitions rightTitle
+            document.getElementById("rightTitle").innerHTML = screenCarousel[1].imgTitle;
+            document.getElementById('rightTitle').className = "";
+            setTimeout(function() {
+                var delayMTitle = document.getElementById('rightTitle').className = "active";
+                delayMTitle;
+            },175);
+
+            //put active object at the end of the array
+            screenCarousel.push(active)
+        }                              //end else
+
+        //this event helps to keep track of which hand was last clicked
+        e = e || event;
+        $.lastClicked = e.target || e.srcElement;
+        var lastClickedElement = $.lastClicked;
+        window.lastClickedElement=lastClickedElement;
+
+    })                     //end left hand
+
+
+
+});                                     //end document ready
+
+
+
+
+
+
+//this following code creates variables I can then call in my HTML
+//    document.getElementById('imageOne').src = screenCarousel[0].imgSrc;
+//    document.getElementById('imageTwo').src = screenCarousel[1].imgSrc;
+//    document.getElementById('imageThree').src = screenCarousel[2].imgSrc;
+
 //
-//        $.get(url, function (data) {
-//            // can use 'data' in here...
-//                        console.log(data);
-//
+
+
 //        });
 
-})
+
+//    $( ".right" ).click(function() {
+//        $(".rightFlick").show();
+//            setTimeout(function() {
+//                $(".rightFlick").hide();
+//                        },175);
+//
+//        $(".right").hide();
+//        setTimeout(function() {
+//            $(".right").show();
+//        },175);
+//        $("#imageOne, #imageTwo").hide()
+//
+//        $("#imageThree").show()
+//
+//
+//    });
+//    $( ".left" ).click(function() {
+//        $(".leftFlick").show();
+//        setTimeout(function() {
+//            $(".leftFlick").hide();
+//        },175);
+//        $(".left").hide();
+//        setTimeout(function() {
+//            $(".left").show();
+//        },175);
+//        $("#imageTwo, #imageThree").hide()
+//        $("#imageOne").show()
+//
+//
+//
+//    });
 
 
 
+
+//$( ".right" ).click(function(e) {
+//    //control snapping fingers
+//    $(".rightFlick").show();
+//    setTimeout(function() {
+//        $(".rightFlick").hide();
+//    },175);
+//
+//    $(".right").hide();
+//    setTimeout(function() {
+//        $(".right").show();
+//    },175);
+//    //end snapping fingers
+//    //keep track if the right hand has been flicked
+//    e = e || event;
+//    $.lastClicked = e.target || e.srcElement;
+//    var lastClickedElement = $.lastClicked;
+//    window.lastClickedElement=lastClickedElement;
+//    console.log(window.lastMousedownElement);
+//    //end right hand has been flicked
+//    //make the current [0] object in array the active object
+//    var activeRight = screenCarousel.shift();
+//    //pull out info from active object
+//    document.getElementById('imageActive').src = activeRight.imgSrc;
+//    console.log(activeRight.imgSrc);
+//    //put active object at the end of the array
+//    screenCarousel.push(activeRight)
+//    console.log(screenCarousel);
+//})                                      //end right Click
+//
+//
+//
+//$( ".left" ).click(function(e) {
+//    //control snapping fingers
+//    $(".leftFlick").show();
+//    setTimeout(function() {
+//        $(".leftFlick").hide();
+//    },175);
+//    $(".left").hide();
+//    setTimeout(function() {
+//        $(".left").show();
+//    },175);
+//    //end snapping fingers
+//    //keep track if the right hand has been flicked
+//    e = e || event;
+//    $.lastMousedown = e.target || e.srcElement;
+//
+//    var lastMousedownElement = $.lastMousedown;
+//    window.lastMousedownElement=lastMousedownElement;
+//
+//
+//    console.log(window.lastClickedElement);
+//
+//    if($(".slider").attr){console.log("yoyo")}
+//
+//    //pull out info from active object
+//    var activeLeft = screenCarousel.pop();
+//    document.getElementById('imageActive').src = activeLeft.imgSrc;
+//
+//    console.log(activeLeft.imgSrc);
+//
+//    //puts active object at the end of the array
+//    screenCarousel.unshift(activeLeft)
+//    console.log(screenCarousel);
+//
+//})
